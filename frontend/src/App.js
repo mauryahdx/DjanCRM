6ad5de32a2103e66f6f1779517538d3c9dddc0ed
Surya Prakash Mourya <suryamourya@iiitdmj.ac.in>
2021-12-25 17:29:28 +0530

    configured tailwind react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="max-w-7xl mx-auto">
-      
+      <Navbar></Navbar>
     </div>
   );
 }
