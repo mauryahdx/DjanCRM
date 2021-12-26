45f867024d9370846c4334d02f8d52c24f80afcf
Surya Prakash Mourya <suryamourya@iiitdmj.ac.in>
2021-12-26 18:54:21 +0530

    rendered django forms via react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Login from './pages/registration/Login'
+import SignUp from './pages/registration/SignUp'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
+    
     <div className="max-w-7xl mx-auto">
+      
+     <BrowserRouter>
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+       <Route path="/" exact element={<Home/>}/>
+       <Route path="/login" exact element={<Login/>}/>
+       <Route path="/signup" exact element={<SignUp/>}/>
+    
+      </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
--- frontend/src/pages/registration/SignUp.jsx
+++ frontend/src/pages/registration/SignUp.jsx
@@ -1,10 +1,42 @@
 
+import { useEffect, useState} from "react";
 
 const SignUp = () => {
+    const [html, setHtml] = useState([]);
+
+    useEffect(() => {
+        fetch('signup')
+        .then(response => response.text())
+        .then(text => {
+            setHtml(text)
+            //var parser = new DOMParser();
+            //setHtml(parser.parseFromString(text, "text/html"));
+         
+        })
+    }, [])
+
+
+
+//var markup
+//var x =0;
+//fetch('login')
+//    .then(res => markup=res.text())
+//    .then(res => {
+//        //console.log(res)
+//        //return res
+//        x=x+1;
+//        console.log(x)
+//       var parser = new DOMParser();
+//       markup = parser.parseFromString(res, "text/html");
+//        console.log(markup)
+//        })
+//    .catch(err => console.error(err));
+//console.log("YO",markup)
+    //"<p>foo bar</p>"<div dangerouslySetInnerHTML={{__html: html}} />
     return (
-        <div>
-            
-        </div>
+     
+        <><div dangerouslySetInnerHTML={{__html: html}} /></>
+    
     )
 }
 
